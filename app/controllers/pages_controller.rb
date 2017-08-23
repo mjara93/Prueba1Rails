12f8fad3afea35dd4e8b87059ac5e00b3b6b5706
Matias Jara <mjara.figueroa@gmail.com>
2017-08-23 10:40:31 -0300

    controller pages y estructura

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,10 @@
+class PagesController < ApplicationController
+  def batman-vs-superman
+  end
+
+  def batman
+  end
+
+  def superman
+  end
+end
---
+++ test/controllers/pages_controller_test.rb
@@ -0,0 +1,19 @@
+require 'test_helper'
+
+class PagesControllerTest < ActionDispatch::IntegrationTest
+  test "should get batman-vs-superman" do
+    get pages_batman-vs-superman_url
+    assert_response :success
+  end
+
+  test "should get batman" do
+    get pages_batman_url
+    assert_response :success
+  end
+
+  test "should get superman" do
+    get pages_superman_url
+    assert_response :success
+  end
+
+end
