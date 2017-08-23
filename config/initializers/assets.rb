a1e7866352e52eedfdab635c1051a2c19e6da1f2
Matias Jara <mjara.figueroa@gmail.com>
2017-08-23 12:54:31 -0300

    agregando imagen a vendor e imagen a page1

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -6,7 +6,8 @@ Rails.application.config.assets.version = '1.0'
 # Add additional assets to the asset load path
  Rails.application.config.assets.paths << Rails.root.join('vendor', 'assets', 'stylesheets')
  Rails.application.config.assets.paths << Rails.root.join('vendor', 'assets', 'javascripts')
+ Rails.application.config.assets.paths << Rails.root.join('vendor', 'assets', 'images')
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-  Rails.application.config.assets.precompile += %w( *.js *.css )
+  Rails.application.config.assets.precompile += %w( *.js *.css *.png *.jpg )
